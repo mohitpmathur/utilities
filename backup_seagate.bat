f99a44f725a2391668caf5c0fe682a0e9fad65e0
mohitpmathur <mohitpmathur@gmail.com>
2018-04-07 15:49:10 -0400

    updating folder exlusion

--- backup_seagate.bat
+++ backup_seagate.bat
@@ -7,7 +7,7 @@ echo ### Backing up Mooney
 ROBOCOPY C:\Users\Mohit\Desktop\Mooney F:\Sunaina\Mooney /e /LOG+:logs\Sunaina_backuplog.txt /NP
 
 echo ### Backing up Mohit
-ROBOCOPY C:\Users\Mohit\Documents\Mohit F:\Mohit /e  /XF *.ova /XF *.iso /XD temp /XD data /XD input /LOG+:logs\Mohit_backuplog.txt /NP
+ROBOCOPY C:\Users\Mohit\Documents\Mohit F:\Mohit /e  /XF *.ova /XF *.iso /XD temp /XD data /XD input /XD submissions /LOG+:logs\Mohit_backuplog.txt /NP
 
 echo ### Backing up Music
 ROBOCOPY C:\Users\Mohit\Music\Hindi F:\Music\Hindi /e /LOG+:logs\Music_backuplog.txt /NP
